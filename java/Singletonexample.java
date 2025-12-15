ec5820637b1c901a9a594f346af22fb9794c1162
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2025-12-15 23:36:29 +0530

    Implement main method in Singleton class
    Added main method to demonstrate Singleton behavior.

--- java/Singletonexample.java
+++ java/Singletonexample.java
@@ -10,4 +10,11 @@ class Singleton {
         }
         return instance;
     }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2);
+    }
 }
