c397611ff4acda7b2dc32de766f5ecac1c1d5258
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2025-12-24 00:03:56 +0530

    Create AtomicIntegerDemo.java

---
+++ java/AtomicIntegerDemo.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerDemo {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                count.incrementAndGet();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + count.get());
+    }
+}
